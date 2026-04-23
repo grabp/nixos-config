e17fd821f942d47be2d3c6560e1bfb77d0dcdb5a
grabp <grabowskip@icloud.com>
2026-04-23 18:28:16 +0200

    feat: added yet another pc (#11)

---
+++ home/patrykgrabowski/dmuchawa/default.nix
@@ -0,0 +1,27 @@
+{
+  nhModules,
+  config,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    "${nhModules}/common"
+    "${nhModules}/darwin"
+  ];
+
+  # Enable home-manager
+  programs.home-manager.enable = true;
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "25.11";
+
+  # xdg.configFile."zellij/layouts/main.kdl".source = ./zellij-layout.kdl;
+  home.file.zellij-layout.source = config.lib.file.mkOutOfStoreSymlink ./zellij-layout.kdl;
+  home.file.zellij-layout.target = "./default.kdl";
+
+  home.packages = with pkgs; [
+    claude-code
+    opencode
+  ];
+}
