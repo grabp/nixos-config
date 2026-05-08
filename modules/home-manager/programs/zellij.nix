fff3d82bed6205be095feb2119cf033243d029db
Patryk Grabowski <grabowskip@icloud.com>
2026-05-09 01:19:24 +0200

    feat: improve zellij config and add zellij-forgot plugin

--- modules/home-manager/programs/zellij.nix
+++ modules/home-manager/programs/zellij.nix
@@ -1,8 +1,19 @@
 { pkgs, ... }:
+let
+  zellijForgot = pkgs.fetchurl {
+    url = "https://github.com/karimould/zellij-forgot/releases/download/0.4.2/zellij_forgot.wasm";
+    hash = "sha256-MRlBRVGdvcEoaFtFb5cDdDePoZ/J2nQvvkoyG6zkSds=";
+  };
+  zellijConfig = pkgs.runCommand "zellij-config" { } ''
+    mkdir -p $out/plugins
+    cp -r ${./zellij}/. $out/
+    cp ${zellijForgot} $out/plugins/zellij-forgot.wasm
+  '';
+in
 {
   home.packages = with pkgs; [
     zellij
   ];
 
-  xdg.configFile."zellij".source = ./zellij;
+  xdg.configFile."zellij".source = zellijConfig;
 }
